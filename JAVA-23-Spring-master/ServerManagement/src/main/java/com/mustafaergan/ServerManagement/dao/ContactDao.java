dbc6763139bb06a55559b5667b381d75b8854cc6
Mustafa ERGAN <mustafaergan@gmail.com>
2017-10-04 22:30:04 +0300

    delete

--- JAVA-23-Spring-master/ServerManagement/src/main/java/com/mustafaergan/ServerManagement/dao/ContactDao.java
+++
@@ -1,9 +0,0 @@
-package com.mustafaergan.ServerManagement.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.mustafaergan.ServerManagement.entity.Contact;
-
-public interface ContactDao extends CrudRepository<Contact, Integer> {
-
-}
